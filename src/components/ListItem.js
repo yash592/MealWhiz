d7a44007a899843d23c80e32f4a53a29a30b5ee3
Yash Rajgor <yash592@gmail.com>
2018-08-06 19:08:25 -0700

    Not working yet

---
+++ src/components/ListItem.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { CardSection } from './common';
+
+
+
+class ListItem extends Component {
+	onRowPress() {
+		Actions.employeeEdit({ employee: this.props.employee });
+	}
+
+
+	render() {
+		const { title } = this.props.recipe
+		return (
+			<TouchableWithoutFeedback>
+				<View>
+					<CardSection>
+						<Text style={styles.titleStyle}>
+							{title}
+						</Text>
+					</CardSection>
+				</View>
+			</TouchableWithoutFeedback>
+
+		);
+	}
+}
+
+const styles = {
+	titleStyle: {
+		fontSize: 18,
+		paddingLeft: 15
+	}
+}
+
+export default ListItem;
